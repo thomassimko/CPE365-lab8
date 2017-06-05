beb52aae2258be4cc61da037270945068bce6bda
Thomas Simko <tjsimko@gmail.com>
2017-06-05 11:43:46 -0700

    stuff

--- DatabaseConnector.java
+++ DatabaseConnector.java
@@ -1,4 +1,37 @@
+import java.sql.*;
 
 public class DatabaseConnector {
 
+	private static Connection conn = null;
+	private String url = "jdbc:mysql://cslvm74.csc.calpoly.edu/tjsimko?user=tjsimko&password=dekhtyar";
+	
+	public DatabaseConnector() {
+		if (conn == null)
+		{
+			establishConnection();
+		}
+	}
+
+	private void establishConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception ex) {
+			System.out.println("Error fetching driver");
+			ex.printStackTrace();
+		};
+
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (Exception ex) {
+			System.out.println("Error establishing connection");
+			ex.printStackTrace();
+		};
+
+		System.out.println("Connected");
+	}
+	
+	public void doSomething() {
+		
+	}
+	
 }
--- Driver.java
+++ Driver.java
@@ -1,4 +1,6 @@
 
 public class Driver {
-
+	public static void main(String[] args) {
+		DatabaseConnector dc = new DatabaseConnector();
+	}
 }

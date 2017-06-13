c5c88dd44680ac3a0e5bcc2df60a76f7a80bf2bd
mdavis60 <mdavis60@calpoly.edu>
2017-06-12 19:58:24 -0700

    general 3

--- Queries.sql
+++ Queries.sql
@@ -41,59 +41,49 @@ LIMIT 10;
 
 -- General Q3
 
-SELECT AbsoluteInc, RelativeInc
-FROM
-   (SELECT AbsoluteInc, @r1 := @r1 + 1 as num
-   FROM
-      (SELECT ticker as AbsoluteInc
-      FROM
-         (SELECT p.ticker, p.open
-         FROM Prices p,
-            (SELECT p.ticker, min(Day) as Day
-            FROM Prices p
-            GROUP BY p.ticker) l
-         WHERE p.ticker = l.ticker and l.Day = p.Day) t1
-
-         NATURAL JOIN
-
-         (SELECT p.ticker, p.close
-         FROM Prices p,
-            (SELECT p.ticker, max(Day) as Day
-            FROM Prices p
-            GROUP BY p.ticker) l
-         WHERE p.ticker = l.ticker and l.Day = p.Day) t2
-      ORDER BY (close - open) DESC
-      LIMIT 5) a,
-
-      (SELECT @r1 := 0) var
-   ) t1,
-
-   (SELECT RelativeInc, @r2 := @r2 + 1 as num
-   FROM
-      (SELECT ticker as RelativeInc
-      FROM
-         (SELECT p.ticker, p.open
-         FROM Prices p,
-            (SELECT p.ticker, min(Day) as Day
-            FROM Prices p
-            GROUP BY p.ticker) l
-         WHERE p.ticker = l.ticker and l.Day = p.Day) t1
-
-         NATURAL JOIN
-
-         (SELECT p.ticker, p.close
-         FROM Prices p,
-            (SELECT p.ticker, max(Day) as Day
-            FROM Prices p
-            GROUP BY p.ticker) l
-         WHERE p.ticker = l.ticker and l.Day = p.Day) t2
-
-      ORDER BY (close / open) DESC
-      LIMIT 5) r,
-
-      (SELECT @r2 := 0) var
-   ) t2
-WHERE t1.num = t2.num;
+SELECT absYears.Year, absYears.Place, absYears.ticker as Absolute, relYears.ticker as Relative
+FROM (SELECT abs1.year, abs1.ticker, abs1.Absolute, count(*) as Place
+   FROM (SELECT tDays.year, tDays.ticker, ap2.Close-ap1.Open as Absolute
+      FROM (((SELECT YEAR(day) as year, ticker, min(day) as YearOpen, max(day) as YearClose
+         FROM AdjustedPrices p
+         GROUP BY year, ticker) tDays JOIN AdjustedPrices ap1 
+            on (tDays.YearOpen=ap1.day and tDays.ticker=ap1.ticker))
+         JOIN AdjustedPrices ap2 ON (tDays.ticker=ap2.ticker and tDays.YearClose=ap2.day))
+      GROUP BY year, ticker) abs1,
+      (SELECT tDays.year, tDays.ticker, ap2.Close-ap1.Open as Absolute
+      FROM (((SELECT YEAR(day) as year, ticker, min(day) as YearOpen, max(day) as YearClose
+         FROM AdjustedPrices p
+         GROUP BY year, ticker) tDays JOIN AdjustedPrices ap1 
+            on (tDays.YearOpen=ap1.day and tDays.ticker=ap1.ticker))
+         JOIN AdjustedPrices ap2 ON (tDays.ticker=ap2.ticker and tDays.YearClose=ap2.day))
+      GROUP BY year, ticker) abs2
+   WHERE abs1.year=abs2.year and abs1.Absolute<=abs2.Absolute
+   GROUP BY abs1.year, abs1.ticker
+   HAVING Place <= 5
+   ORDER BY abs1.year, abs1.Absolute, Place DESC
+   ) absYears
+JOIN
+   (SELECT rel1.year, rel1.ticker, rel1.Relative, count(*) as Place
+   FROM (SELECT tDays.year, tDays.ticker, 100*(ap2.Close/ap1.Open) as Relative
+      FROM (((SELECT YEAR(day) as year, ticker, min(day) as YearOpen, max(day) as YearClose
+         FROM AdjustedPrices p
+         GROUP BY year, ticker) tDays JOIN AdjustedPrices ap1 
+            on (tDays.YearOpen=ap1.day and tDays.ticker=ap1.ticker))
+         JOIN AdjustedPrices ap2 ON (tDays.ticker=ap2.ticker and tDays.YearClose=ap2.day))
+      GROUP BY year, ticker) rel1,
+      (SELECT tDays.year, tDays.ticker, 100*(ap2.Close/ap1.Open) as Relative
+      FROM (((SELECT YEAR(day) as year, ticker, min(day) as YearOpen, max(day) as YearClose
+         FROM AdjustedPrices p
+         GROUP BY year, ticker) tDays JOIN AdjustedPrices ap1 
+            on (tDays.YearOpen=ap1.day and tDays.ticker=ap1.ticker))
+         JOIN AdjustedPrices ap2 ON (tDays.ticker=ap2.ticker and tDays.YearClose=ap2.day))
+      GROUP BY year, ticker) rel2
+   WHERE rel1.year=rel2.year and rel1.Relative<=rel2.Relative
+   GROUP BY rel1.year, rel1.ticker
+   HAVING Place <= 5
+   ORDER BY rel1.year, Place DESC
+   )relYears on (absYears.year=relYears.Year and absYears.Place=relYears.Place)
+ORDER BY year, Place;
 
 -- Individual
 SELECT s.ticker, s.name, MIN(p.DAY), MAX(p.Day)
--- src/TableTest.java
+++ src/TableTest.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import static org.junit.Assert.*;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 
 public class TableTest extends TestCase {
 	
+	@Test
 	public void test()
 	{
 		

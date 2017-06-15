27faad3573ff1aa5afc41de11dfd519686cf8be2
Thomas Simko <tjsimko@gmail.com>
2017-06-14 17:15:37 -0700

    makefile

---
+++ testDir/DatabaseConnector.java
@@ -0,0 +1,113 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DatabaseConnector {
+
+	private static Connection conn = null;
+	private String url;
+	
+	public DatabaseConnector(String login, String pass, String db) {
+		url = "jdbc:mysql://cslvm74.csc.calpoly.edu/" + db + "?user=" + login + "&password=" + pass;
+		if (conn == null)
+		{
+			establishConnection();
+		}
+	}
+
+	private void establishConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception ex) {
+			System.out.println("Error fetching driver");
+			ex.printStackTrace();
+		};
+	
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (Exception ex) {
+			System.out.println("Error establishing connection");
+			ex.printStackTrace();
+		};
+
+		System.out.println("Connected");
+	}
+	
+	public void closeConnection() {
+		try {
+            conn.close();
+            conn = null;
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Unable to close connection");
+        };
+	}
+	
+	public Connection getConnection() {
+		if (conn == null)
+		{
+			establishConnection();
+		}
+		return conn;
+	}
+	
+	public List<HashMap<String, Object>> resultSetToTuples(ResultSet rs)
+	{
+		List<HashMap<String,Object>> result = new ArrayList<HashMap<String,Object>>();
+
+		try {
+			ResultSetMetaData md = (ResultSetMetaData) rs.getMetaData();
+			int col = md.getColumnCount();
+	
+			while (rs.next()) {
+				HashMap<String, Object> row = new HashMap<String, Object>(col);
+				for(int i = 1; i <= col; ++i) {
+					
+					Object obj = rs.getObject(i);
+					row.put(md.getColumnName(i), obj);
+				}
+				result.add(row);
+			}
+	
+			rs.close();
+		} catch (Exception ex){
+			ex.printStackTrace();
+		}
+		return result;
+	}
+	
+	public List<List<String>> tuplesToList(List<HashMap<String, Object>> tuples, List<String> columns) {
+		ArrayList<List<String>> ret = new ArrayList<List<String>>();
+		for(HashMap<String,Object> tuple : tuples) {
+			
+			ret.add(tupleToString(tuple, columns));
+		}
+		return ret;
+	}
+	private List<String> tupleToString(HashMap<String,Object> tuple, List<String> columns) {
+		ArrayList<String> ret = new ArrayList<String>();
+		for(String key: columns) {
+			ret.add(tuple.get(key).toString());
+		}
+		return ret;
+	}
+	
+	public List<String> getColumnNames(ResultSet rs) {
+		ArrayList<String> columns = new ArrayList<String>();
+		ResultSetMetaData md;
+		try {
+			md = (ResultSetMetaData) rs.getMetaData();
+			int col = md.getColumnCount();
+			for(int i = 1; i <= col; ++i) {
+				columns.add(md.getColumnName(i));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return columns;
+	}
+	
+}
---
+++ testDir/Driver.java
@@ -0,0 +1,199 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+
+//KLAC, DISCK, NWL, SYY
+
+
+public class Driver {
+	public static void main(String[] args) {
+
+		System.out.println(args[0]);
+		String ticker = args[0];
+
+		Credentials cred = new Credentials();
+		DatabaseConnector dc = new DatabaseConnector(cred.getLogin(), cred.getPass(), cred.getDB());
+
+		String ticker2;
+		if (ticker.equals("KLAC")) {
+			ticker2 = "DISCK";
+		}
+		else
+			ticker2 = "KLAC";
+		Individual ind = new Individual(dc, ticker, ticker2);
+		General gen = new General(dc);
+		HtmlWriter html = new HtmlWriter(args[0], "KLAC Analysis");
+		
+		Analyzer anal = new Analyzer();
+
+		html.addSection("General");
+
+		//Query 1
+
+		html.addHeading("1. 2016 Security Trading Breakdown");
+		Table g1 = new Table();
+		List<String> g1Columns = dc.getColumnNames(gen.getR1());
+		g1.addColumns(g1Columns);
+		List<List<String>> grs1 = dc.tuplesToList(dc.resultSetToTuples(gen.getR1()), g1Columns);
+		for (List<String> tuple : grs1) {
+			g1.addRow(tuple);
+		}
+		html.addTable(g1.getTable());
+		html.addText("Reports the total number of securities traded at the start of 2016, total number of securities traded at the end of 2016, total number " 
+				+ "of securities whose prices saw increase between the end of 2015 and the end of 2016, and the total number of securities whose prices saw "
+				+ "decrease between the end of 2015 and the end of 2016.");
+
+
+		//Query 2
+
+		html.addHeading("2. 10 Most Traded Stocks of 2016");
+		Table g2 = new Table();
+		List<String> g2Columns = dc.getColumnNames(gen.getR2());
+		g2.addColumns(g2Columns);
+		List<List<String>> grs2 = dc.tuplesToList(dc.resultSetToTuples(gen.getR2()), g2Columns);
+		for (List<String> tuple : grs2) {
+			g2.addRow(tuple);
+		}
+		html.addTable(g2.getTable());
+		html.addText("");
+
+
+		//Query 3
+
+		html.addHeading("3. 5 Top Performing Stocks Per Year");
+		Table g3 = new Table();
+		List<String> g3Columns = dc.getColumnNames(gen.getR3());
+		g3.addColumns(g3Columns);
+		List<List<String>> grs3 = dc.tuplesToList(dc.resultSetToTuples(gen.getR3()), g3Columns);
+		for (List<String> tuple : grs3) {
+			g3.addRow(tuple);
+		}
+		html.addTable(g3.getTable());
+		html.addText("For each year, reports top five highest performing stocks in terms of "
+				+ "the absolute price increase and top five highest performing stocks in terms of relative price increase."
+				+ "  The resulting table is ordered by place.");
+
+
+
+		//Query 4
+
+		html.addHeading("4. 10 Stocks to Watch in 2017");
+		Table g4 = new Table();
+		List<String> g4Columns = dc.getColumnNames(gen.getR4());
+		g4.addColumns(g4Columns);
+		List<List<String>> grs4 = dc.tuplesToList(dc.resultSetToTuples(gen.getR4()), g4Columns);
+		for (List<String> tuple : grs4) {
+			g4.addRow(tuple);
+		}
+		html.addTable(g4.getTable());
+		html.addText("Reports the 10 stocks with the greatest relative price increase in 2016");
+
+		//Query 5	
+		html.addHeading("5. 2016 Report per Sector");
+		Table g5 = new Table();
+		List<String> g5Columns = new ArrayList<String>();
+		g5Columns.add("Sector");
+		g5Columns.add("Report");
+		g5.addColumns(g5Columns);
+		List<List<String>> grs5 = anal.gen5(dc.resultSetToTuples(gen.getR5()));
+		for (List<String> tuple : grs5) {
+			g5.addRow(tuple);
+		}
+		html.addTable(g5.getTable());
+		html.addText("Reports are calculated based on the sector's relative growth in comparison to the market's relative growth.");
+
+
+
+		html.addSection("Individual");
+		//Query 1
+
+		html.addHeading("1. " + ticker + " Pricing Date Range");
+		Table t1 = new Table();
+		List<String> t1Columns = dc.getColumnNames(ind.getR1());
+		t1.addColumns(t1Columns);
+		List<List<String>> rs1 = dc.tuplesToList(dc.resultSetToTuples(ind.getR1()), t1Columns);
+		for (List<String> tuple : rs1) {
+			t1.addRow(tuple);
+		}
+		html.addTable(t1.getTable());
+		html.addText("For the given ticker, displays the name of the ticker and the first day of sales and last day of sales in the database.");
+
+		//Query 2
+
+		html.addHeading("2. " + ticker + " Stock Performance Per Year");
+		Table t2 = new Table();
+		List<String> t2Columns = dc.getColumnNames(ind.getR2());
+		t2.addColumns(t2Columns);
+		List<List<String>> rs2 = dc.tuplesToList(dc.resultSetToTuples(ind.getR2()), t2Columns);
+		for (List<String> tuple : rs2) {
+			t2.addRow(tuple);
+		}
+		html.addTable(t2.getTable());
+		html.addText("Reports the increase/decrease in prices year-over-year, volume of trading, average closing price in a given year, average trade volume per day.");
+
+		//Query 3
+
+		html.addHeading("3. " + ticker + " 2016 Monthly Breakdown");
+		Table t3 = new Table();
+		List<String> t3Columns = dc.getColumnNames(ind.getR3());
+		t3.addColumns(t3Columns);
+		List<List<String>> rs3 = dc.tuplesToList(dc.resultSetToTuples(ind.getR3()), t3Columns);
+		for (List<String> tuple : rs3) {
+			t3.addRow(tuple);
+		}
+		html.addTable(t3.getTable());
+		html.addText("Reports the average closing price, the highest and the lowest price, the average daily trading volume by month.");
+
+
+		//Query 4
+		html.addHeading("4. " + ticker + " Best Month Per Year");
+		Table t4 = new Table();
+		List<String> t4Columns = dc.getColumnNames(ind.getR4());
+		t4.addColumns(t4Columns);
+		List<List<String>> rs4 = dc.tuplesToList(dc.resultSetToTuples(ind.getR4()), t4Columns);
+		for (List<String> tuple : rs4) {
+			t4.addRow(tuple);
+		}
+		html.addTable(t4.getTable());
+		html.addText("compares the stocks relative performance to the sector and industry average relative performance to\n determine the best performance of the stock by month");
+
+		
+		
+		
+		//Query 7
+		
+		html.addHeading("7. " + ticker + " vs. Top Five of 2016");
+		Table t7 = new Table();
+		List<String> t7Columns = dc.getColumnNames(ind.getR7());
+		t7.addColumns(t7Columns);
+		List<List<String>> rs7 = dc.tuplesToList(dc.resultSetToTuples(ind.getR7()), t7Columns);
+		for (List<String> tuple : rs7) {
+			t7.addRow(tuple);
+		}
+		html.addTable(t7.getTable());
+		html.addText("The top 5 are calculated based on relative growth in 2016. Values are calculated as {" + ticker + "} - {topFive}.");
+		
+		
+		//Query 8
+		
+		html.addHeading("8. " + ticker + " vs. " +  ticker2);
+		Table t8 = new Table();
+		List<String> t8Columns = dc.getColumnNames(ind.getR8());
+		t8.addColumns(t8Columns);
+		List<HashMap<String, Object>> r8 = dc.resultSetToTuples(ind.getR8());
+		List<List<String>> rs8 = dc.tuplesToList(r8, t8Columns);
+		for (List<String> tuple : rs8) {
+			t8.addRow(tuple);
+		}
+		html.addTable(t8.getTable());
+		html.addText(anal.indiv8(r8, ticker, ticker2));
+		
+		
+		html.publishHtml();
+		
+	}
+}
